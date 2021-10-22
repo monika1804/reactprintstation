6ae3292e51e6d5063708a5bbce76f7e35862082d
monika <rajputmonika276@gmail.com>
2021-10-22 11:24:41 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Home/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Navbar />
     </div>
   );
 }
