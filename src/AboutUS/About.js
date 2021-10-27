b94c547d294f47626fb1e1335cc21d6603d57e96
sheetal1005 <sheetalyada927@gmail.com>
2021-10-27 20:42:38 +0530

    my first  code

--- src/AboutUS/About.js
+++ src/AboutUS/About.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
-export default function About() {
+
+function About() {
   return (
-    <div>
-      <h1>hi i m About</h1>
+    <div className="about">
+      <div class="container">
+          backgroundImage: `url("https://via.placeholder.com/500")`
+      <div class="col-lg-5">
+            <h1 class="font-weight-light">About</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+export default About;
